0a2ec4723ef66c99ecbee1d4d278d9c4d72ecbda
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2024-06-04 23:38:53 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: '',
-      component: import('../views/AboutView.vue')
+      component: import('../views/HomeView.vue')
     },
     {
       path: '/skills',
